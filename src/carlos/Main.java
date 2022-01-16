3f4a60bd414f3efdf3ab547c012d6d6c77936556
Carlos Milkovic <carlos.milkovi@gmail.com>
2022-01-16 00:26:58 +0000

    demo version.

--- src/carlos/Main.java
+++ src/carlos/Main.java
@@ -7,6 +7,7 @@ import carlos.webscraper.service.ScraperService;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Scanner;
 
 import static carlos.webscraper.Option.SAVE_LINKS;
 import static carlos.webscraper.Option.SAVE_PARSED_ELEMENTS;
@@ -20,21 +21,30 @@ public class Main {
         return length >= 3 && length <= 10;
     }
     public static void main(String[] args) {
-        var text = StandardParser.TEXT.getWithLimitAndFilter(10_000_000, Main::between3And10Words);
-        var service = ScraperService.singletonService();
-        service.add(() -> WebScraperBuilder.of(text)
-                        .withThreadPoolSize(4)
-                        .withOptions(SAVE_PARSED_ELEMENTS, SAVE_LINKS),
-                "https://www.boards.ie",
-                "https://www.reddit.com",
-                "https://www.irishtimes.com");
-        service.add(() -> WebScraperBuilder.of(text)
-                .withThreadPoolSize(8)
-                .restrictLanguage(ENGLISH)
-                .withOptions(SAVE_PARSED_ELEMENTS, SAVE_LINKS),
-                "https://en.wikipedia.org");
-        service.startAsync()
-                .whenComplete((s, e) -> runComparison(text)).join();
+        System.out.println("PROJECT DEMO");
+        System.out.println("Enter the number of sentences you would like to compare" +
+                "(15 000 or less if you dont want to wait a long time):");
+        try {
+            var text = StandardParser.TEXT.getWithLimitAndFilter(new Scanner(System.in).nextInt(), Main::between3And10Words);
+            var service = ScraperService.singletonService();
+            service.add(() -> WebScraperBuilder.of(text)
+                            .withThreadPoolSize(4)
+                            .withOptions(SAVE_PARSED_ELEMENTS, SAVE_LINKS),
+                    "https://www.boards.ie",
+                    "https://www.reddit.com",
+                    "https://www.irishtimes.com");
+            service.add(() -> WebScraperBuilder.of(text)
+                            .withThreadPoolSize(8)
+                            .restrictLanguage(ENGLISH)
+                            .withOptions(SAVE_PARSED_ELEMENTS, SAVE_LINKS),
+                    "https://en.wikipedia.org");
+            service.startAsync()
+                    .whenComplete((s, e) -> runComparison(text)).join();
+        }
+        catch(Exception e) {
+            System.err.println("your argument should be a single number!");
+            main(null);
+        }
     }
     private static void runComparison(HTMLParser text) {
         System.out.println("-------------------");
